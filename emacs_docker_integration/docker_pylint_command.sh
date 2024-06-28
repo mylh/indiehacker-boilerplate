54fccc9c659989d605e56b31c506a18e4ae3c3ec
Dmitry S <mylh@pm.me>
2024-06-28 17:04:36 +0200

    imporove pylint integration within dev container

--- emacs_docker_integration/docker_pylint_command.sh
+++ emacs_docker_integration/docker_pylint_command.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-output=$(docker exec -i $CONTAINER_NAME pylint $1 "$2" ${3/$PROJECT_ROOT/$CONTAINER_ROOT} ${4/$PROJECT_ROOT/$CONTAINER_ROOT} ${5/$PROJECT_ROOT/$CONTAINER_ROOT} ${6/$PROJECT_ROOT/$CONTAINER_ROOT})
-echo $output
-exit 0
+
+# Ensure CONTAINER_NAME is set
+if [ -z "$CONTAINER_NAME" ]; then
+    echo "Error: CONTAINER_NAME is not set"
+    exit 1
+fi
+
+# Prepare the arguments for pylint
+args=()
+for arg in "$@"; do
+    if [[ $arg == $PROJECT_ROOT* ]]; then
+        args+=("${arg/$PROJECT_ROOT/$CONTAINER_ROOT}")
+    else
+        args+=("$arg")
+    fi
+done
+
+# Run pylint in the Docker container
+output=$(docker exec -i "$CONTAINER_NAME" pylint "${args[@]}")
+
+# Output the result
+echo "$output"
+
+# Exit with the same status code as pylint
+exit $?
