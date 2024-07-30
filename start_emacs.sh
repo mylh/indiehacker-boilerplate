18781235f9ee13a22345dc9ec4d86e1a6638247e
Dmitry S <mylh@pm.me>
2024-07-30 09:50:42 +0200

    start emacs and detach from console

--- start_emacs.sh
+++ start_emacs.sh
@@ -2,5 +2,5 @@ export CONTAINER_NAME={{project_name}}_web
 export PROJECT_ROOT=$(pwd)
 export CONTAINER_ROOT=/home/user/app
 DESKTOP_DIR=$(pwd)
-emacs --eval="(desktop-change-dir \"$DESKTOP_DIR\")"
+emacs --eval="(desktop-change-dir \"$DESKTOP_DIR\")" &
 
